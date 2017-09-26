42bd482f82f70c6a58433e7af0bb70672596915b
foogaro <l.fugaro@gmail.com>
2017-09-26 13:54:52 +0200

    Micro vdb lab

---
+++ jdv/prj/backend/src/main/java/com/redhat/rhte/data/DataAccess.java
@@ -0,0 +1,7 @@
+package com.redhat.rhte.data;
+
+/**
+ * Created by foogaro on 26/09/17.
+ */
+public class DataAccess {
+}
---
+++ jdv/prj/backend/src/main/java/com/redhat/rhte/model/Person.java
@@ -0,0 +1,7 @@
+package com.redhat.rhte.rs;
+
+/**
+ * Created by foogaro on 15/09/17.
+ */
+public class Person {
+}
